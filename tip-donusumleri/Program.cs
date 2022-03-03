4064ca1f9e1acf3ac55317dc12dfed9bb3112c17
Emre Kaya <59964776+emrekya021@users.noreply.github.com>
2022-03-03 21:18:58 +0300

    Add files via upload

---
+++ tip-donusumleri/Program.cs
@@ -0,0 +1,20 @@
+﻿//Tip Dönüşümleri
+
+//Explict Conservation (bilinçli dönüşümler)
+
+int x = 5;
+byte y = (byte)x;
+
+Console.WriteLine(y);
+
+string c = "Merhaba";
+object d = (string)c;
+
+Console.WriteLine(d);
+
+
+// ToString Methodu
+
+int xx = 6 ;
+string yy = xx.ToString();
+Console.WriteLine(yy);
