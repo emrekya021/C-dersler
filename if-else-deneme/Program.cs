b80e54d0cc3bdfb8df585ce99e48ad5fe6fc1b85
Emre Kaya <59964776+emrekya021@users.noreply.github.com>
2022-03-03 21:55:26 +0300

    Add files via upload

---
+++ if-else-deneme/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Selam seçilmiş kişi eğer saat 21:00'den önce ise oyunu kazandın");
+
+int time = DateTime.Now.Hour;
+
+if(time<21){
+    Console.WriteLine("Kazandınnn :D");
+}else
+{
+    Console.WriteLine("Kaybettin yarına kadar bekle :[");
+}
+
+Console.WriteLine("-------------------xxxxxxxxxxxxxx-------------------");
+
+int time2 = DateOnly.MaxValue.Day;
+
+Console.WriteLine(time2);
+
+Console.WriteLine("-------------------xxxxxxxxxxxxxx-------------------");
+
+int saat = DateTime.Now.Hour;
+
+if(saat == 21){
+    Console.WriteLine("Atam Dolma Bahçede");
+}else{
+    Console.WriteLine("Başka bir zaman geri gel");
+}
