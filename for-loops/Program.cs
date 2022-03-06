e2e718e0507914f6b7788f1e13be14f99ffb030b
Emre Kaya <59964776+emrekya021@users.noreply.github.com>
2022-03-06 23:07:20 +0300

    Add files via upload

---
+++ for-loops/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine("Bir Sayı Giriniz ;");
+
+int sayi = int.Parse(Console.ReadLine());
+
+int teksayi = 0;
+
+for (int i = 0; i < sayi; i++)
+{
+    if (i%2 == 1)
+    {
+        teksayi += i;  
+    
+    }
+
+}
+    Console.WriteLine(teksayi); 
+
+Console.WriteLine("--------------***************----------------");
+
+for (int a = 1; a < 10; a++)
+{
+    
+    if(a == 4)
+    break;
+    Console.WriteLine(a);
+    
+}
+
+Console.WriteLine("--------------***************----------------");
+
+for (int a = 1; a < 10; a++)
+{
+    
+    if(a == 4)
+    continue;
+    Console.WriteLine(a);
+    
+}
