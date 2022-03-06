3b4fc02b825cfc9ff4a20c14dd74206e2e001ab1
Emre Kaya <59964776+emrekya021@users.noreply.github.com>
2022-03-07 01:43:05 +0300

    Add files via upload

---
+++ method/Program.cs
@@ -0,0 +1,22 @@
+﻿// Methodlar
+
+// a ve b yi topla
+
+int a = 5 ;
+int b = 4;
+
+
+int sonuc = topla(a,b);
+
+ekrana_yazdır(Convert.ToString(sonuc));
+
+
+int topla(int deger1,int deger2)
+{
+    return(deger1+deger2);
+}
+
+
+void ekrana_yazdır(object yazi){
+    Console.WriteLine(yazi);
+}
