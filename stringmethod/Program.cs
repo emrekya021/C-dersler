ec02c6b2b5119ca34e84cb42f6f7170cef46c27c
Emre Kaya <59964776+emrekya021@users.noreply.github.com>
2022-03-09 17:53:38 +0300

    Add files via upload

---
+++ stringmethod/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string yazi = "Stardew Valley";
+string yazi1 = "Hoşgeldin";
+
+// Yazıdaki karakter sayısını belirtir Length
+Console.WriteLine(yazi.Length);
+
+// Ya hep ya hiç ToUpper , ToLower
+Console.WriteLine(yazi.ToUpper());
+Console.WriteLine(yazi1.ToLower());
+
+// Remove
+Console.WriteLine(yazi.Remove(4,5));
+Console.WriteLine(yazi1.Remove(6));
+
+// Replace
+Console.WriteLine(yazi.Replace("Star","*"));
+
+// Split (Önemli!!!)
+Console.WriteLine(yazi.Split(' ')[1]);
